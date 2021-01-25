65dada9469aecc5b6e3c763652b5720073539de0
Harish Nishad <harish.nishad0775@gmail.com>
2021-01-25 12:59:23 +0530

    add Ignou file:

---
+++ IGNOU/readingURL.java
@@ -0,0 +1,18 @@
+package IGNOU;
+import java.net.*;
+
+
+import java.io.*;
+public class readingURL {
+
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://www.oracle.com/");
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+    }
+}
