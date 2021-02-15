6b7c0ec52ec407727382430e7f5fa9ec2428da0c
Harish Nishad <harish.nishad0775@gmail.com>
2021-02-15 22:33:49 +0530

    add Demo File

---
+++ Applet/demo.java
@@ -0,0 +1,12 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class demo extends Applet{
+     public void paint(Graphics g)
+     {
+           g.drawString("Greeky Shows", 50, 20);
+     }
+}
+/* 
+<applet code="HelloWorld" width=200 height=60> 
+</applet> 
+*/
