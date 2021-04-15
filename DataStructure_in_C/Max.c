19e224d55287fa19fd34167b4ed37cf9a87f2f12
Harish Nishad <harish.nishad0775@gmail.com>
2021-04-15 21:13:16 +0530

    add file

--- DataStructure_in_C/Max.c
+++ DataStructure_in_C/Max.c
@@ -10,7 +10,7 @@
             int i;
             struct Node *t,*last;
             first=(struct Node *)malloc(sizeof(struct Node));
-            first->data=A[0];
+            first->data=A[0]; 
             first->next=NULL;
             last=first;
 
