b6ef073424c1211eaff7fe4dc3b3f57563a4ac08
Harish Nishad <harish.nishad0775@gmail.com>
2021-09-04 21:58:55 +0530

    Error in code || while getstate

--- React_Development_practice/movie-cart-app/src/index.js
+++ React_Development_practice/movie-cart-app/src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './components/App';
 import './index.css';
+import movies from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(movies)
+
+console.log('store',store);
+console.log('Before-STORE',store.getState());
+
+store.dispatch({
+    type : 'ADD Movie',
+    movie : [{name : 'Superman'}]
+})
+console.log('Afer-STORE',store.getState());
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
+
+
