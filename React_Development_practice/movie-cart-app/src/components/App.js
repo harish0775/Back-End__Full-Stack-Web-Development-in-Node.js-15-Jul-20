97117ae81988acb7f83cd50268eb2f2dea0ab61a
Harish Nishad <harish.nishad0775@gmail.com>
2021-09-04 19:22:36 +0530

    Add UI to Movoie Card:

--- React_Development_practice/movie-cart-app/src/components/App.js
+++ React_Development_practice/movie-cart-app/src/components/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React from 'react';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+import { data } from '../data';
+
 function App() {
   return (
     <div className="App">
-         Happy Hacking
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div id="list">
+          {data.map(movie => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
